895bcd7092066d4adb3d474a9fd6b375835ed81e
BSick7 <brad.sickles@gmail.com>
2014-05-28 18:03:47 -0400

    Created ActiveTemplate and EditTemplate dependency property that will be used in GridCell Template.  When IsEditing=true, ActiveTemplate=EditTemplate.  When IsEditing=false, ActiveTemplate=ContentTemplate.

--- Source/Fayde.Experimental/GridCell.ts
+++ Source/Fayde.Experimental/GridCell.ts
@@ -3,7 +3,23 @@ module Fayde.Experimental {
         static IsEditingProperty = DependencyProperty.Register("IsEditing", () => Boolean, GridCell, false, (d, args) => (<GridCell>d).OnIsEditingChanged(args.OldValue, args.NewValue));
         IsEditing: boolean;
         private OnIsEditingChanged(oldIsEditing: boolean, newIsEditing: boolean) {
+            this.SetCurrentValue(GridCell.ActiveTemplateProperty, newIsEditing ? this.EditTemplate : this.ContentTemplate);
+        }
+        
+        static EditTemplateProperty = DependencyProperty.Register("EditTemplate", () => DataTemplate, GridCell, undefined, (d, args) => (<GridCell>d).OnEditTemplateChanged(args.OldValue, args.NewValue));
+        EditTemplate: DataTemplate;
+        OnEditTemplateChanged(oldTemplate: DataTemplate, newTemplate: DataTemplate) {
+            if (this.IsEditing)
+                this.SetCurrentValue(GridCell.ActiveTemplateProperty, newTemplate);
+        }
 
+        static ActiveTemplateProperty = DependencyProperty.RegisterReadOnly("ActiveTemplate", () => DataTemplate, GridCell);
+        ActiveTemplate: DataTemplate;
+        
+        OnContentTemplateChanged(oldContentTemplate: DataTemplate, newContentTemplate: DataTemplate) {
+            super.OnContentTemplateChanged(oldContentTemplate, newContentTemplate);
+            if (!this.IsEditing)
+                this.SetCurrentValue(GridCell.ActiveTemplateProperty, newContentTemplate);
         }
 
         constructor() {
