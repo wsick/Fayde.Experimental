4206de74f740c86854a27795e1b033e48e1bd362
BSick7 <brad.sickles@gmail.com>
2014-05-29 11:26:35 -0400

    Added change notification to items so that 2 way bindings work in test.

---
+++ Source/Fayde.Experimental.Test/Models/TestItem.ts
@@ -0,0 +1,13 @@
+﻿class TestItem extends Fayde.MVVM.ObservableObject {
+    FirstName: string;
+    LastName: string;
+    Age: number;
+    constructor(firstName?: string, lastName?: string, age?: number) {
+        super();
+        this.FirstName = firstName;
+        this.LastName = lastName;
+        this.Age = age;
+    }
+}
+Fayde.MVVM.NotifyProperties(TestItem, ["FirstName", "LastName", "Age"]);
+export = TestItem; 
--- Source/Fayde.Experimental.Test/ViewModels/MainViewModel.ts
+++ Source/Fayde.Experimental.Test/ViewModels/MainViewModel.ts
@@ -1,30 +1,16 @@
 /// <reference path="../lib/Fayde/Fayde.d.ts" />
 
+import TestItem = require("../Models/TestItem");
+
 class MainViewModel extends Fayde.MVVM.ViewModelBase {
-    Items = new Fayde.Collections.ObservableCollection();
+    Items = new Fayde.Collections.ObservableCollection<TestItem>();
     constructor() {
         super();
         this.Items.AddRange([
-            {
-                FirstName: "First1",
-                LastName: "Last1",
-                Age: 10
-            },
-            {
-                FirstName: "First2",
-                LastName: "Last2",
-                Age: 12
-            },
-            {
-                FirstName: "First3",
-                LastName: "Last3",
-                Age: 13
-            },
-            {
-                FirstName: "First4",
-                LastName: "Last4",
-                Age: 14
-            }
+            new TestItem("First1", "Last1", 10),
+            new TestItem("First2", "Last2", 12),
+            new TestItem("First3", "Last3", 13),
+            new TestItem("First4", "Last4", 14)
         ]);
     }
     RemoveItem(args: Fayde.IEventBindingArgs<Fayde.RoutedEventArgs>) {
