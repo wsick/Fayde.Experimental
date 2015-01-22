3d936710d3fe0a04970103abe825a5e2bde02524
BSick7 <brad.sickles@gmail.com>
2015-01-22 16:31:34 -0500

    Adding themes.

--- dist/fayde.experimental.js
+++ dist/fayde.experimental.js
@@ -2,7 +2,7 @@ var Fayde;
 (function (Fayde) {
     var Experimental;
     (function (Experimental) {
-        Experimental.Version = '0.6.0';
+        Experimental.Version = '0.6.1';
     })(Experimental = Fayde.Experimental || (Fayde.Experimental = {}));
 })(Fayde || (Fayde = {}));
 var __extends = this.__extends || function (d, b) {
